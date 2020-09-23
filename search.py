2fd979b12f466a11c198ee7b14a91ce554f1345a
eled47 <ele2117@columbia.edu>
2020-09-23 15:09:58 -0400

    Updated Search.py

--- search.py
+++ search.py
@@ -5,3 +5,11 @@ import time
 driver = webdriver.Chrome()
 
 driver.get("https://techwithtim.net")
+
+search = driver.find_element_by_name("s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+time.sleep(30)
+
+driver.quit()
