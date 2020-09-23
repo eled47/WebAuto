a92d22cbfedd6d7e472f223c933dd9735f79cdb0
eled47 <ele2117@columbia.edu>
2020-09-23 12:50:02 -0400

    Completed Automation of Looking up my Youtube Channel

--- automation.py
+++ automation.py
@@ -6,5 +6,12 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 driver.get('https://youtube.com')
 
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="search-input"]'))).send_keys("youtube test")
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='search_query']"))).send_keys("EL47")
 driver.find_element_by_xpath("//button[@id='search-icon-legacy']/yt-icon").click()
+
+# searchbox = driver.find_element_by_xpath('//div[@id="search-input"]')
+# searchbox.send_keys('EL47')
+
+# searchbutton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+# searchbutton.click()
+
